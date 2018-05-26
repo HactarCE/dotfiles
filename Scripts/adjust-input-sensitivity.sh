bcece4ab93f6a880896b2077fbbec347d915bd69
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-05-25 21:38:39 -0400

    Some desktop-specific fixes for Compton and mouse sensitivity

--- Scripts/adjust-input-sensitivity.sh
+++ Scripts/adjust-input-sensitivity.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 xset r rate 300 30 # key repeat delay
-xinput set-prop "Razer Razer Naga 2014" 286 -0.7 # mouse sensitivity (might need to change 10 to 9)
+xinput set-prop "pointer:Logitech Gaming Mouse G600" 297 -1.0 # mouse sensitivity
 numlockx # enable numlock
