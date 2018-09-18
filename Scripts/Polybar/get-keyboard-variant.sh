1ef2789352d2c4360c11c0ca51c868ba3728963d
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-09-18 09:17:38 -0400

    Added ~/Scripts/Polybar/get-keyboard-variant.sh

---
+++ Scripts/Polybar/get-keyboard-variant.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+# while true; do
+    if setxkbmap -query | grep 'variant:' >/dev/null; then
+        variant=$(setxkbmap -query | grep variant | cut -d: -f2)
+        variant=$(echo $variant) # kill extaneous whitespace
+        echo "${variant^}" # capitalize first letter
+    else
+        echo " Qwerty"
+    fi
+#     watch -gn 0.2 setxkbmap -query &>/dev/null
+# done
