97e17926aa00eeb49b2cff4ccd74d30b768c8195
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-12-09 01:56:48 -0500

    Added a script and some aliases to make sudoing more bearable

---
+++ Scripts/update-root-configurations.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+sudo rm -r /root/.config/autorandr
+sudo rm -r /root/.oh-my-zsh
+sudo rm /root/.zsh_aliases
+sudo rm /root/.zshrc
+
+sudo cp -r $HOME/.config/autorandr /root/.config
+sudo cp -r $HOME/.oh-my-zsh /root
+sudo cp $HOME/.zsh_aliases /root
+sudo cp $HOME/.zshrc /root
+
+sudo sed -i "s/$(whoami)/root/g" /root/.zshrc
+sudo sed -i 's/\/home\/root/\/root/g' /root/.zshrc
