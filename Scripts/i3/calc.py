25578b18003abec82eb558f05649b1b10c2c9a60
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-02 00:29:05 -0500

    Generalized numlock doublepress code and cleaned up ~/Scripts/i3/calc.py a bit

--- Scripts/i3/calc.py
+++ Scripts/i3/calc.py
@@ -19,55 +19,37 @@ import os
 import utils
 
 TEMPFILE = path.join(path.dirname(path.abspath(__file__)), TEMPFILENAME)
-print(TEMPFILE)
 
-def await_doublepress(timeout=0.25):
-    try:
-        with open(TEMPFILE, 'r+') as f:
-            presses = int(f.read())
-            f.seek(0)
-            f.write(str(presses + 1))
-            exit()
-    except:
-        with open(TEMPFILE, 'w') as f:
-            presses = 1
-            f.write("1")
-    try:
-        call(['inotifywait', '-e', 'modify', TEMPFILE], timeout=DOUBLEPRESS_TIMEOUT)
-        return True
-    except TimeoutExpired:
-        return False
-    finally:
-        os.remove(TEMPFILE)
-
-is_running = not call(XDOTOOL_SEARCH_COMMAND)
-if is_running:
-    win_id = check_output(XDOTOOL_SEARCH_COMMAND).splitlines()[0]
-    print('hi')
-    print(win_id)
-    if utils.win_is_mapped(win_id):
-        print('map')
-        if utils.win_is_visible(win_id):
-            print('vis')
-            if utils.win_is_active(win_id):
-                print('act')
-                if await_doublepress():
-                    print('double!')
-                    # If double-pressed when active, close it
-                    utils.win_close(win_id)
+with utils.MultiPressChecker(TEMPFILE) as multipress:
+    if not multipress.first:
+        exit()
+    is_running = not call(XDOTOOL_SEARCH_COMMAND)
+    if is_running:
+        win_id = check_output(XDOTOOL_SEARCH_COMMAND).splitlines()[0]
+        print('found', win_id)
+        if utils.win_is_mapped(win_id):
+            print('mapped')
+            if utils.win_is_visible(win_id):
+                print('visisble')
+                if utils.win_is_active(win_id):
+                    print('active')
+                    # If pressed when active, unmap it
+                    utils.win_unmap(win_id, False)
+                    if multipress.wait(2):
+                        print('double-pressed')
+                        # If double-pressed when active, close it
+                        utils.win_close(win_id)
                 else:
-                    # If single-pressed when active, unmap it
-                    utils.win_unmap(win_id)
+                    # If not active, focus it
+                    utils.win_focus(win_id)
             else:
-                # If not active, focus it
+                # If not visible, focus it
                 utils.win_focus(win_id)
         else:
-            # If not visible, focus it
-            utils.win_focus(win_id)
-    else:
-        # If not mapped, map it
-        utils.win_map(win_id)
-elif await_doublepress():
-    # If not running, start the program on double-press
-    # i3_exec is not portable to other WMs/DEs :(
-    utils.i3_exec(CALC_COMMAND)
+            # If not mapped, map it
+            utils.win_map(win_id, False)
+    elif multipress.wait(2):
+        # If not running, start the program on double-press
+        # i3_exec is not portable to other WMs/DEs :(
+        utils.i3_exec(CALC_COMMAND)
+        pass
--- Scripts/i3/utils.py
+++ Scripts/i3/utils.py
@@ -1,5 +1,8 @@
-from subprocess import call, check_output, CalledProcessError
+#!/usr/bin/env python3
+
+from subprocess import call, check_output, CalledProcessError, TimeoutExpired
 from shlex import split as shellsplit, quote as shellquote
+from time import time
 import i3ipc
 import os
 import re
@@ -101,14 +104,14 @@ def win_close(win_id=None):
 def win_kill(win_id=None):
     return xdotool(['windowkill', win_id or get_active_window_id()])
 
-def win_focus(win_id):
-    return xdotool(['windowfocus', '--sync', win_id])
+def win_focus(win_id, sync=True):
+    return xdotool(('windowfocus' + (' --sync' if sync else '')).split() + [win_id])
 
-def win_map(win_id):
-    return xdotool(['windowmap', '--sync', win_id])
+def win_map(win_id, sync=True):
+    return xdotool(('windowmap' + (' --sync' if sync else '')).split() + [win_id])
 
-def win_unmap(win_id):
-    return xdotool(['windowunmap', '--sync', win_id])
+def win_unmap(win_id, sync=True):
+    return xdotool(('windowunmap' + (' --sync' if sync else '')).split() + [win_id])
 
 def win_hacky_unfocus(win_id=None):
     if win_id == None:
@@ -118,3 +121,64 @@ def win_hacky_unfocus(win_id=None):
     new_focus = get_active_window_id()
     win_map(win_id)
     xdotool(['windowactivate', new_focus], True)
+
+
+
+class MultiPressChecker:
+
+    def __init__(self, filename, timeout=0.5):
+        self.filename = filename
+        self.timeout = timeout
+        self.presses = None
+
+    def __enter__(self):
+        try:
+            with open(self.filename, 'r+') as f:
+                self.read_data(f)
+                if abs(time() - self.time) > self.timeout * 2:
+                    raise Exception("File is too old")
+                self.presses += 1
+                self.write_data(f)
+            self.first = False
+        except:
+            self.first = True
+            with open(self.filename, 'w') as f:
+                self.presses = 1
+                self.time = time()
+                self.write_data(f)
+        return self
+
+    def read_data(self, fileobject):
+        f = fileobject
+        filetime, filepresses = f.read().split()
+        self.time = float(filetime)
+        self.presses = int(filepresses)
+
+    def write_data(self, fileobject):
+        f = fileobject
+        f.seek(0)
+        f.write(str(self.time) + ' ' + str(self.presses))
+        f.truncate()
+
+    def get_wait_time(self):
+        with open(self.filename) as f:
+            self.read_data(f)
+        return self.time + self.timeout - time()
+
+    def wait(self, press_count=None):
+        print(self.presses)
+        try:
+            wait_time = self.get_wait_time()
+            while wait_time > 0:
+                if press_count and self.presses >= press_count:
+                    return True
+                call(['inotifywait', '-e', 'modify', self.filename], timeout=wait_time)
+                wait_time = self.get_wait_time()
+        except TimeoutExpired:
+            return press_count and self.presses >= press_count
+
+    def __exit__(self, type, value, traceback):
+        if self.first:
+            if self.timeout > 0:
+                self.wait()
+            os.remove(self.filename)
