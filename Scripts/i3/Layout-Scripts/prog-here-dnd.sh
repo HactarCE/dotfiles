638f5ab1665e3f42c077cafcfcea1e6930bfdd01
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-02 16:21:42 -0500

    Added items list to prog-here-dnd.sh

--- Scripts/i3/Layout-Scripts/prog-here-dnd.sh
+++ Scripts/i3/Layout-Scripts/prog-here-dnd.sh
@@ -6,6 +6,6 @@ xtoolwait termite .
 i3 "move up; focus down"
 xtoolwait subl -n . ./*/*.txt
 i3 "focus up"
-xtoolwait google-chrome-stable --new-window https://twinelab.net/spellbook/ ./*.pdf
+xtoolwait google-chrome-stable --new-window https://twinelab.net/spellbook/ https://roll20.net/compendium/dnd5e/Items%20List#content ./*.pdf
 i3 "move container right; focus left"
 xdotool getactivewindow windowkill
