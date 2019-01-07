44bef04681388a4fafcabc555f3cb88b027247e8
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-01-07 13:48:17 -0500

    Added prog-here-awesome.sh

---
+++ Scripts/i3/Layout-Scripts/prog-here-awesome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/.config/awesome
+
+i3 "move container to workspace $(basename $(pwd)); workspace $(basename $(pwd)); layout tabbed; split v; layout stacked"
+xtoolwait subl3 -n . rc.lua
+xtoolwait google-chrome-stable --new-window https://www.lua.org/pil/1.html https://awesomewm.org/doc/
+i3 "move container right; focus left"
+
+clear
