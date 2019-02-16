0c2ddeec573826d3b369f3ae6c74262a0f71e9d6
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-02-15 22:03:17 -0500

    Rmeoved ~/Scripts/adjust-input-sensitivity.sh, but added two scripts to work around an X server bug

--- Scripts/adjust-input-sensitivity.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xset r rate 300 30 # key repeat delay
-xinput set-prop "pointer:Logitech Gaming Mouse G600" 297 -1.0 # mouse sensitivity
-numlockx # enable numlock
---
+++ Scripts/mouse-sens-normal.sh
@@ -0,0 +1,1 @@
+xinput --set-prop 'Logitech Gaming Mouse G600' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 2.5
