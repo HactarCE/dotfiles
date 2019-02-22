0138abf96d1d88a7f7cce5ee6464de7710dc7ebe
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-02-21 23:17:10 -0500

    Removed some unnecessary lines from the Tetris remapping script

--- Scripts/tetrisfriends-controls.sh
+++ Scripts/tetrisfriends-controls.sh
@@ -9,14 +9,4 @@ xmodmap -e "keycode 44 = z"     # j   n
 xmodmap -e "keycode 45 = x"     # k   e
 xmodmap -e "keycode 47 = c"     # ;   o
 
-# # Reverse mappings to make i3wm happy
-# # (assumes Colemak)
-# xmodmap -e "keycode 65 = w"
-# xmodmap -e "keycode 113 = a"
-# xmodmap -e "keycode 116 = r"
-# xmodmap -e "keycode 114 = s"
-# xmodmap -e "keycode 52 = n"
-# xmodmap -e "keycode 53 = e"
-# xmodmap -e "keycode 54 = o"
-
 i3-msg reload
