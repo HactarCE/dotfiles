5f658ab78105f810e6dab2f05c46cf7f93e44fc6
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-03-13 13:37:34 -0400

    Updated prog-here-dnd.sh

--- Scripts/i3/Layout-Scripts/prog-here-dnd.sh
+++ Scripts/i3/Layout-Scripts/prog-here-dnd.sh
@@ -4,8 +4,8 @@ cd ~/Dropbox/Games/Dungeons-and-Dragons/
 i3 "move container to workspace D&D; workspace D&D; layout stacked"
 xtoolwait termite .
 i3 "move up; focus down"
-xtoolwait subl3 -n . ./*/*.txt
+xtoolwait subl3 -n .
 i3 "focus up"
-xtoolwait google-chrome-stable --new-window https://twinelab.net/spellbook/ https://roll20.net/compendium/dnd5e/Items%20List#content ./*.pdf
+xtoolwait google-chrome-stable --new-window https://twinelab.net/spellbook/ https://roll20.net/compendium/dnd5e/Items%20List#content ./PDFs/*.pdf
 i3 "move container right; focus left"
 xdotool getactivewindow windowkill
