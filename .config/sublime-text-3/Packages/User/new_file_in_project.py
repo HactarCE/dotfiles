16d2a6dcf16de7240551e3b338e817f3673c8379
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-03-13 13:36:37 -0400

    Fixed new_file_in_project.py

--- .config/sublime-text-3/Packages/User/new_file_in_project.py
+++ .config/sublime-text-3/Packages/User/new_file_in_project.py
@@ -4,5 +4,8 @@ import sublime_plugin
 
 class NewFileListener(sublime_plugin.EventListener):
     def on_new_async(self, view):
-        if view.window() and view.window().folders():
-            view.settings().set('default_dir', view.window().folders()[0])
+        window = view.window()
+        if window and window.folders():
+            settings = view.settings()
+            if not settings.get('default_dir'):
+                settings.set('default_dir', window.folders()[0])
