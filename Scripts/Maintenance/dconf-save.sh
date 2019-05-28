257b80e25f90068d393b828184b593ce195589bb
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-05-27 22:40:44 -0400

    Added Onboard settings

--- Scripts/Maintenance/dconf-save.sh
+++ Scripts/Maintenance/dconf-save.sh
@@ -16,4 +16,8 @@ crudini --get --format=ini tmp-dump 'org/nemo/plugins' >> nemo
 echo >> nemo
 crudini --get --format=ini tmp-dump 'org/nemo/sidebar-panels/tree' >> nemo
 
+crudini --get --format=ini tmp-dump 'org/onboard' >> onboard
+crudini --get --format=ini tmp-dump 'org/onboard/keyboard' >> onboard
+crudini --get --format=ini tmp-dump 'org/onboard/theme-settings' >> onboard
+
 rm ~/.config/dconf/user.d/tmp-dump
