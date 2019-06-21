62d66c674090a2a0d782f6a6165c5612d19fd633
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-06-21 01:26:09 -0400

    Added prog-here-discordpy.sh

---
+++ Scripts/i3/Layout-Scripts/prog-here-discordpy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+i3 "move container to workspace $(basename $(pwd)); workspace $(basename $(pwd)); layout tabbed; split v; layout stacked"
+xtoolwait subl3 -n .
+xtoolwait termite -e "zsh -c 'while true; do python; clear; done'" -t python
+i3 "focus up"
+xtoolwait google-chrome-stable --new-window https://discordpy.readthedocs.io/en/stable/api.html https://discordpy.readthedocs.io/en/stable/ext/commands/api.html
+i3 "move container right; focus left"
+
+clear
