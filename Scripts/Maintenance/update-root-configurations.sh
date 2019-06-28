c999f2f4370b3637daf828cf725002daafce1a5e
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-06-27 20:36:44 -0400

    Updated update-root-configurations.sh to use Antigen

--- Scripts/Maintenance/update-root-configurations.sh
+++ Scripts/Maintenance/update-root-configurations.sh
@@ -3,14 +3,14 @@
 cd "$HOME"
 
 sudo rm -r /root/.config/autorandr
-sudo rm -r /root/.oh-my-zsh
 sudo rm /root/.zsh_aliases
 sudo rm /root/.zshrc
 
 sudo cp -r $HOME/.config/autorandr /root/.config
-sudo cp -r $HOME/.oh-my-zsh /root
 sudo cp $HOME/.zsh_aliases /root
 sudo cp $HOME/.zshrc /root
+
+sudo [ -d /root/.antigen ] || sudo git clone https://github.com/zsh-users/antigen.git /root/.antigen
 
 sudo sed -i "s/$(whoami)/root/g" /root/.zshrc
 sudo sed -i 's/\/home\/root/\/root/g' /root/.zshrc
