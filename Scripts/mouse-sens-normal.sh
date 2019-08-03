4876b27e9ae38ec4a813dde0c38348c5ba32037d
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-08-02 20:55:12 -0400

    Mouse sensitivity scripts now work on laptop

--- Scripts/mouse-sens-normal.sh
+++ Scripts/mouse-sens-normal.sh
@@ -1,1 +1,3 @@
 xinput --set-prop 'Logitech Gaming Mouse G600' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 2.5
+xinput --set-prop 'USB Optical Mouse' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 2.5
+xinput --set-prop 'ELAN0501:00 04F3:3060 Touchpad' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 2.5
