28f9c9ed591119663c5be55f171da052cd1f36bb
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-08-03 16:16:50 -0400

    'Save as...' now starts in the project directory

--- .config/sublime-text-3/Packages/User/new_file_in_project.py
+++ .config/sublime-text-3/Packages/User/new_file_in_project.py
@@ -1,6 +1,7 @@
-# from https://forum.sublimetext.com/t/default-folder-to-save-new-files/14812/4
+# based on https://forum.sublimetext.com/t/default-folder-to-save-new-files/14812/4
 
 import sublime_plugin
+
 
 class NewFileListener(sublime_plugin.EventListener):
     def on_new_async(self, view):
@@ -9,3 +10,10 @@ class NewFileListener(sublime_plugin.EventListener):
             settings = view.settings()
             if not settings.get('default_dir'):
                 settings.set('default_dir', window.folders()[0])
+
+
+class PromptSaveInProject(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.settings().set('default_dir', self.view.window().folders()[0])
+        self.view.run_command('prompt_save_as')
+        print('oh')
