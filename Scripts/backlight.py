d6a199859108bc431cdb64b064c78c34ccad8cce
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-08-26 22:02:18 -0400

    Add an even dimmer backlight setting (0.2%)

--- Scripts/backlight.py
+++ Scripts/backlight.py
@@ -5,12 +5,11 @@ from time import sleep
 import sys
 
 
-SETTINGS = [0.5, 1, 2, 3, 5, 10, 15, 20, 30, 50, 75, 100]
+SETTINGS = [0.2, 0.5, 1, 2, 3, 5, 10, 15, 20, 30, 50, 75, 100]
 
 args = sys.argv[1:]
 
-output = check_output(['xbacklight', '-get'])
-# output = check_output(['light', '-G'])
+output = check_output(['xbacklight', '-getf'])
 
 try:
     backlight = float(output)
@@ -32,6 +31,5 @@ clamp = lambda *args: sorted(args)[1]
 backlight = SETTINGS[clamp(0, i, len(SETTINGS) - 1)]
 
 call(['xbacklight', '-set', str(backlight), '-fps', '60', '-time', '100'])
-# for i in range(2): # because polybar updates too quickly
 sleep(0.02)
 call(['xbacklight', '-set', str(backlight)])
