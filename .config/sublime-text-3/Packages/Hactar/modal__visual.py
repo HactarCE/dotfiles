1e04900fb18ae7d98dd09bb1591402814ad619ee
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-09-12 14:56:43 -0400

    Simplify modal__visual.py

--- .config/sublime-text-3/Packages/Hactar/modal__visual.py
+++ .config/sublime-text-3/Packages/Hactar/modal__visual.py
@@ -1,15 +1,12 @@
 from os import path
 import os
-import imp
 
-# from . import modal__config as config
-# config = imp.reload(config)
 from .modal__config import (
     VISUAL_DIRNAME,
     VISUAL_TEMPLATE,
     STATE_SETTINGS,
     DEFAULT_STATE,
-)  # noqa: E402
+)
 
 
 VISUAL_DIR = path.join(path.dirname(__file__), VISUAL_DIRNAME)
@@ -23,12 +20,6 @@ try:
     os.symlink(r'/dev/shm/ST3-edit-states', VISUAL_DIR)
 except OSError:
     pass
-
-# try:
-#     for file in os.listdir(VISUAL_DIR):
-#         os.remove(path.join(VISUAL_DIR, file))
-# except OSError:
-#     pass
 
 
 def update_state(view):
