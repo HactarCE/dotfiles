517b981569580cb4923abbd413577701c44efa04
HactarCE <6060305+HactarCE@users.noreply.github.com>
2026-08-28 17:34:22 -0400

    Add back fzf keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -181,3 +181,45 @@ function ymdmv --description "ymdmv <filename>"
         mv "$arg" (ymdmod "$arg")_"$arg"
     end
 end
+
+# ---
+
+function __fzf_git_sh
+    # Get the absolute path to the parent directory of this script (i.e. the
+    # parent directory of fzf-git.sh) to use in the key bindings to avoid
+    # having to modify `$PATH`.
+    set --function fzf_git_sh_path (realpath (status dirname))
+
+    commandline --insert (SHELL=bash bash "$fzf_git_sh_path/fzf-git.sh" --run $argv | string join ' ')
+    commandline --function repaint
+end
+
+set --local commands branches each_ref files hashes lreflogs remotes stashes tags worktrees
+
+for command in $commands
+    set --function key (string sub --length=1 $command)
+
+    eval "bind -M default \cg$key   '__fzf_git_sh $command'"
+    eval "bind -M insert  \cg$key   '__fzf_git_sh $command'"
+    eval "bind -M default \cg\c$key '__fzf_git_sh $command'"
+    eval "bind -M insert  \cg\c$key '__fzf_git_sh $command'"
+end
+
+set -gx BAT_PAGER 'less -FR -#1'
+set -gx PAGER bat
+
+# echo "CTRL-G ? to show this list"
+# echo "CTRL-G CTRL-F for Files"
+# echo "CTRL-G CTRL-B for Branches"
+# echo "CTRL-G CTRL-T for Tags"
+# echo "CTRL-G CTRL-R for Remotes"
+# echo "CTRL-G CTRL-H for commit Hashes"
+# echo "CTRL-G CTRL-S for Stashes"
+# echo "CTRL-G CTRL-L for reflogs"
+# echo "CTRL-G CTRL-W for Worktrees"
+# echo "CTRL-G CTRL-E for Each ref (git for-each-ref)"
+# echo
+
+# # For recording
+# set -g fish_history "" # don't store history
+# cd ~/videos
